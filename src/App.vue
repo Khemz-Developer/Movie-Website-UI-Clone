7bb7e14ea22f568a4e31b07b56b71fb222da31c9
Khemz-Developer <jalithapramod@gmail.com>
2025-08-02 19:43:27 +0530

    feat: Implement Hero.vue with slider, navigation dots, and responsive styles

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<!-- <template>
+  <div id="app">
+    <Header />
+    <Hero />
+    <Introduction />
+    <MovieGrid />
+    <ContactMap />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Introduction from './components/Introduction.vue'
+import MovieGrid from './components/MovieGrid.vue'
+import ContactMap from './components/ContactMap.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Hero,
+    Introduction,
+    MovieGrid,
+    ContactMap,
+    Footer
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+</style> -->
+<template>
+  <div id="app">
+    <Header />
+    <Hero />
+    <Introduction />
+    <MovieGrid />
+    <ContactMap />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Introduction from './components/Introduction.vue'
+import MovieGrid from './components/MovieGrid.vue'
+import ContactMap from './components/ContactMap.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Hero,
+    Introduction,
+    MovieGrid,
+    ContactMap,
+    Footer
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Inter', sans-serif;
+  line-height: 1.6;
+  color: #333;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <Header />
+  <Hero />
+  <Introduction />
+  <MovieGrid />
+  <ContactMap />
+  <Footer />
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Hero from '../components/Hero.vue'
+import Introduction from '../components/Introduction.vue'
+import MovieGrid from '../components/MovieGrid.vue'
+import ContactMap from '../components/ContactMap.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Hero,
+    Introduction,
+    MovieGrid,
+    ContactMap,
+    Footer
+  }
+}
+</script>
